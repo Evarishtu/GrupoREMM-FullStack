81c2db75f00eac5c2642f204b6d16bf2f9a5286c
roqueCMBirt <rcarrillomar@uoc.edu>
2025-10-25 16:43:57 +0200

    modificacion en arrays de data.js

--- js/data.js
+++ js/data.js
@@ -1,35 +1,57 @@
-let voluntariados = [];
+let voluntariados = [
+    {
+        titulo: 'Se ofrece chica para pasear perro',
+        usuario: 'Meritxell Mosquera van den Bergh',
+        fecha: '2025-01-10',
+        descripcion: 'Chica responsable se ofrece para pasear perro los martes por la tarde',
+        tipo: 'oferta',
+        imagenFondo: 'https://www.hillspet.com/content/dam/cp-sites-aem/hills/hills-pet/legacy-articles/inset/beagle-with-tongue-out.jpg'
+    },
+    {
+        titulo: 'Se ofrece chico para pasear perro',
+        usuario: 'Roque Carrillo Martinez',
+        fecha: '2025-09-10',
+        descripcion: 'Chico responsable se ofrece para pasear perro los martes por la tarde',
+        tipo: 'oferta',
+        imagenFondo: 'https://image.petmd.com/files/inline-images/shiba-inu-black-and-tan-colors.jpg?VersionId=pLq84BEOjdMjXeDCUJJJLFPuIWYsVMUU'
+    },
+    {
+        titulo: 'Se necesita persona para cuidar de una gata',
+        usuario: 'Evarishtu Dongua Kuzin',
+        fecha: '2025-05-10',
+        descripcion: 'Se busca persona responsable para cuidar de una gata. Su ama ya no puede hacerse cargo',
+        tipo: 'peticion',
+        imagenFondo: 'https://www.minino.com/wp-content/uploads/2025/01/nota-de-blog-31-enero.png.webp'
+    },
+    {
+        titulo: 'Se necesita persona para adoptción de animal',
+        usuario: 'Marc Espuga Moreno',
+        fecha: '2025-07-10',
+        descripcion: 'Se busca persona responsable para adoptar de una iguana. Con conocimientos sobre reptiles',
+        tipo: 'peticion',
+        imagenFondo: 'https://15f8034cdff6595cbfa1-1dd67c28d3aade9d3442ee99310d18bd.ssl.cf3.rackcdn.com/uploaded_thumb_big/c1dc328c546f572dfe66453867eeffb8/cuidar_iguana_domestica_consejos_clinica_veterinaria_la_granja_aviles.png'
+    }
+];
 
 let usuarios = [
     {
-        nombre: 'edongua',
+        nombre: 'Evarishtu Dongua Kuzin',
         email: 'edongua@uoc.edu',
-        password: '12345'
+        password: 'ABCD1234'
+    },
+      {
+        nombre: 'Marc Espuga Moreno',
+        email: 'mespugam@uoc.edu',
+        password: '4321DCBA'
     },
     {
-        nombre: 'marta',
-        email: 'marta@uoc.edu',
-        password: '12345'
+        nombre: 'Meritxell Mosquera van den Bergh',
+        email: 'mmosquerava@uoc.edu',
+        password: '5678ZXWV'
+    },
+      {
+        nombre: 'Roque Carrillo Martínez',
+        email: 'rcarrillomar@uoc.edu',
+        password: 'ZXWV5678'
     }
-]
-
-let ofertas = [
-    {
-        titulo: "Se ofrece persona para cuidar de animales domésticos",
-        fecha: "06-02-2025",
-        descripcion: "Gente responsable se ofrece para cuidar aniamles en caso de ausencia del dueño por un periodo de tiempo",
-        user: "Set"
-    },
-    {
-        titulo: "Se ofrece chico para cuidar de animales domésticos",
-        fecha: "14-08-2025",
-        descripcion: "Chico responsable se ofrece para cuidar aniamles en caso de ausencia del dueño por un periodo de tiempo",
-        user: "Parr"
-    },
-    {
-        titulo: "Se ofrece chica para cuidar de animales domésticos",
-        fecha: "28-12-2025",
-        descripcion: "Chica responsable se ofrece para cuidar aniamles en caso de ausencia del dueño por un periodo de tiempo",
-        user: "Nera"
-    },
-]
+];
